33a4f704e2b6e245bb2e2424508b00d23cffcbba
jihoon <wlgnsdl1016@naver.com>
2024-07-08 10:47:13 +0900

    10926

---
+++ 백준/단계별문제/Bronze/B5_10926.java
@@ -0,0 +1,21 @@
+package 단계별문제.Bronze;
+
+import java.util.*;
+import java.io.*;
+
+public class B5_10926 {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(id);
+        sb.append("??!");
+
+        System.out.println(sb);
+
+
+    }
+}
