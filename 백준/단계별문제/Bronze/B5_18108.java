359ce36e53e4e20be2bbc9d80bf5c1be5f146617
jihoon <wlgnsdl1016@naver.com>
2024-07-08 10:56:14 +0900

    18108

---
+++ 백준/단계별문제/Bronze/B5_18108.java
@@ -0,0 +1,15 @@
+package 단계별문제.Bronze;
+
+import java.util.*;
+import java.io.*;
+
+public class B5_18108 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine())-543);
+
+    }
+}
