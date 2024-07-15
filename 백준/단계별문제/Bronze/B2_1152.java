42dec173f99c23392cedb9670cb5a1d45ac527d5
jihoon <wlgnsdl1016@naver.com>
2024-07-15 10:42:45 +0900

    1152

---
+++ 백준/단계별문제/Bronze/B2_1152.java
@@ -0,0 +1,12 @@
+package 단계별문제.Bronze;
+
+import java.util.*;
+import java.io.*;
+public class B2_1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        System.out.print(st.countTokens());
+    }
+}
